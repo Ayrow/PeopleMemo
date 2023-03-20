9b1ecf5101d4484289f33572aade3a0e2396bd81
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-20 08:28:07 +0100

    Initial Commit

---
+++ PeopleMemo/PeopleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleMemoApp.swift
+//  PeopleMemo
+//
+//  Created by Aymeric Pilaert on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
